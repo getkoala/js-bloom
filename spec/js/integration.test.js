06f61a27ec147c2a44662d65c2fafe24c98f2633
Netto Farah <nettofarah@gmail.com>
2021-12-28 17:39:49 -0300

    Improve integration tests

--- spec/js/integration.test.js
+++ spec/js/integration.test.js
@@ -15,4 +15,21 @@ describe("Integration", () => {
     expect(bloom.test("bruna")).toBe(true);
     expect(bloom.test("tido")).toBe(false);
   });
+
+  it("can be hydrated from a large ruby bloom", async () => {
+    const fixture = (
+      await fs.readFile("./spec/fixtures/bloom-companies.json", "utf8")
+    ).toString();
+
+    const serialized = JSON.parse(fixture);
+    const bloom = new JSBloom(serialized);
+
+    expect(bloom.test("Segment")).toBe(true);
+    expect(bloom.test("Salesforce")).toBe(true);
+    expect(bloom.test("Makelog")).toBe(true);
+
+    expect(bloom.test("Random Co")).toBe(false);
+    expect(bloom.test("Does not exist")).toBe(false);
+    expect(bloom.test("404")).toBe(false);
+  });
 });
--- spec/ruby/integration_spec.rb
+++ spec/ruby/integration_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require 'csv'
 
 describe "Integration" do
   it "should serialize a filter to be used in JS" do
@@ -15,5 +16,36 @@ describe "Integration" do
     expect(bloom_json.test 'matt').to be true
     expect(bloom_json.test 'netto').to be true
     expect(bloom_json.test 'bruna').to be true
+  end
+
+  it "should serialize large blue filters" do
+    companies = CSV.read('./spec/fixtures/companies.csv').map do |row|
+      JSON.parse(row[0])
+    end
+
+    bloom = JsBloom.build(companies.size * 10, 0.001)
+
+    companies.each do |company|
+      bloom.add(company['name'])
+    end
+
+    File.write('./spec/fixtures/bloom-companies.json', bloom.to_json)
+
+    bloom_json = JsBloom.new JSON.parse(File.read('./spec/fixtures/bloom-companies.json'))
+
+    companies.each do |company|
+      expect(bloom_json.test company['name']).to be true
+    end
+
+    expect(bloom_json.test 'Rando').to be false
+    expect(bloom_json.test 'Random Co').to be false
+    expect(bloom_json.test 'Does not exist').to be false
+    expect(bloom_json.test '404').to be false
+
+    config = bloom.to_json
+    gzipped = Zlib::Deflate.deflate(config)
+
+    expect(config.bytesize).to be <= 26_000
+    expect(gzipped.bytesize).to be <= 8_000
   end
 end
