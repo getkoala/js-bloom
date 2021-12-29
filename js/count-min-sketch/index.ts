a198d6a9ada37446c0b4546c32380dbd91e5ca63
Netto Farah <nettofarah@gmail.com>
2021-12-29 14:24:11 -0300

    Basic implementation in JS

---
+++ js/count-min-sketch/index.ts
@@ -0,0 +1,97 @@
+import { crc32 } from "../zlib";
+
+interface CountMinSketchOptions {
+  size: number;
+  hashes: number;
+  data?: number[][];
+  seeds?: number[];
+}
+
+const MAX_FIXNUM = 2 ** (8 * 8 - 2) - 1;
+
+function createData(k: number, m: number): Uint32Array[] {
+  const data = new Array(k);
+  for (let i = 0; i < k; i++) {
+    data[i] = new Uint32Array(m);
+  }
+  return data;
+}
+
+function convertData(data: number[][]): Uint32Array[] {
+  return data.map((k) => Uint32Array.from(k));
+}
+
+function toArray(data: Uint32Array[]): number[][] {
+  return data.map((k) => Array.from(k));
+}
+
+function seeds(k: number): number[] {
+  const seeds = new Array(k);
+  for (let i = 0; i < k; i++) {
+    seeds[i] = Math.random() * (MAX_FIXNUM + 1);
+  }
+  return seeds;
+}
+
+export class CountMinSketch {
+  static DEFAULTS: Partial<CountMinSketchOptions> = {
+    size: 100,
+    hashes: 4,
+  };
+
+  private options: CountMinSketchOptions;
+  private k: number;
+  private m: number;
+  private data: Uint32Array[];
+  private seeds: number[];
+
+  constructor(
+    options: Partial<CountMinSketchOptions> = CountMinSketch.DEFAULTS
+  ) {
+    this.options = {
+      ...CountMinSketch.DEFAULTS,
+      ...options,
+    } as CountMinSketchOptions;
+
+    this.k = this.options.hashes;
+    this.m = this.options.size;
+    this.data = this.options.data
+      ? convertData(this.options.data)
+      : createData(this.k, this.m);
+
+    this.seeds = this.options.seeds || seeds(this.k);
+  }
+
+  count(item: string): number {
+    return this.add(item, 0);
+  }
+
+  add(item: string, n: number = 1): number {
+    let minCount = Infinity;
+
+    this.seeds.forEach((seed, i) => {
+      const hash = crc32(`${item}:${seed + i}`);
+
+      const j = hash % this.m;
+      const cnt = (this.data[i][j] += n);
+
+      if (cnt < minCount) {
+        minCount = cnt;
+      }
+    });
+
+    return minCount;
+  }
+
+  toHash(): object {
+    return {
+      ...this.options,
+      data: toArray(this.data),
+      seeds: this.seeds,
+    };
+  }
+
+  toJSON(): string {
+    return JSON.stringify(this.toHash());
+  }
+}
